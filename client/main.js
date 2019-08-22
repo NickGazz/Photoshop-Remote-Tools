2ed813c146b46018fb6909b05643b474445c4398
Nick Gazzillo <nickgazzillo@bernhardt.com>
2019-08-21 22:29:26 -0400

    Add auth token requesting to photoshop client

--- client/main.js
+++ client/main.js
@@ -4,12 +4,14 @@ var csInterface = new CSInterface();
 path = csInterface.getSystemPath(SystemPath.EXTENSION);
 csInterface.evalScript("new File('"+path+"/host/start_server').execute()");
 
-let auth = window.cep.fs.readFile(path+'/client/jwt').data;
+let auth = window.cep.fs.readFile(path+'/client/auth_token').data;
 
 const ws = io.connect('ws://192.168.1.146:8001', { query: { token: auth } });
 
-
-ws.send('Hi');
-ws.emit('customeEvent', 'This is a test event');
-
-ws.emit('message', 'Test message');
+document.querySelector('#request-code').addEventListener('click', () => {ws.emit('Request Token');} );
+ws.on('New Token', (token) => {
+    const qr = qrcode(0, 'L');
+    qr.addData(token);
+    qr.make();
+    document.getElementById('placeHolder').innerHTML = qr.createImgTag();
+});
