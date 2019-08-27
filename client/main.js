d30da8593b1dc113a8e9e05d0d3555370bb02dd7
Nick Gazzillo <nickgazzillo@bernhardt.com>
2019-08-27 13:07:44 -0400

    Switch from ip to localhost for photoshop ws

--- client/main.js
+++ client/main.js
@@ -6,9 +6,10 @@ csInterface.evalScript("new File('"+path+"/host/start_server').execute()");
 
 let auth = window.cep.fs.readFile(path+'/client/auth_token').data;
 
-const ws = io.connect('ws://192.168.1.146:8001', { query: { token: auth } });
+const ws = io.connect('ws://localhost:8001', { query: { token: auth } });
 
 document.querySelector('#request-code').addEventListener('click', () => {ws.emit('Request Token');} );
+
 ws.on('New Token', (token) => {
     const qr = qrcode(0, 'L');
     qr.addData(token);
