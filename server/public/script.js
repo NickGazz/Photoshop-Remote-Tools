4e101c62a0d959d9865e32722fc1a572e8a6901c
Nick Gazzillo <nickgazzillo@bernhardt.com>
2019-09-13 12:32:06 -0400

    Add tool icons to remote client

--- server/public/script.js
+++ server/public/script.js
@@ -8,9 +8,9 @@ function establishConnection(){
     socket.on('connect', () => main(socket));
     socket.on('Tool Change', tool => {
         Array.from(tools.children).forEach(el => {
-            el.style.background="";
+            el.classList.remove('selected');
         });
-        tools.querySelector(`#${tool}`).style.background="blue";
+        tools.querySelector(`#${tool}`).classList.add('selected');
         // console.log(data);
     });
 }
@@ -21,11 +21,9 @@ function displayTools(){
 function main(socket){
     connectButton.style.display = 'none';
     tools.style.display = '';
-
+    
     tools.addEventListener('click', function(e){
         console.log(e.target.id);
         socket.emit('Tool Change', `${e.target.id}`);
     });
-    
-
 }
