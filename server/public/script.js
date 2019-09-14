272ef660b1eb696fa5ce02110510df78d91c86cf
Nick Gazzillo <nickgazzillo@bernhardt.com>
2019-09-14 19:14:57 -0400

    Dynamically load tool svgs from single sprite file

--- server/public/script.js
+++ server/public/script.js
@@ -1,17 +1,34 @@
 function main(socket){
-    const tools = document.querySelector('#tools');
+    const toolsDiv = document.querySelector('#tools');
+    tools = {};
+    fetch('images/tools-sprite.svg')
+    .then(res => res.text())
+    .then(svgText => {
+        const svgDoc = new DOMParser().parseFromString(svgText, 'image/svg+xml');
+        svgDoc.querySelectorAll('symbol').forEach(symbol => {
+            const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+            svg.innerHTML = symbol.innerHTML;
+            svg.setAttribute('viewBox', symbol.getAttribute('viewBox'));
+            svg.setAttribute('id', symbol.id);
+            toolsDiv.appendChild(svg);
+            tools[symbol.id] = svg;
+            svg.addEventListener('click', (e) => {
+                toolChange(symbol.id, true);
+            });
+        });
+    }).catch(err => {console.error(err)});
+    toolsDiv.style.display="";
 
-    tools.style.display = '';
-    
-    tools.addEventListener('click', function(e){
-        socket.emit('Tool Change', `${e.target.id}`);
-    });
-
-    socket.on('Tool Change', tool => {
-        Array.from(tools.children).forEach(el => {
+    function toolChange(tool, emit = false){
+        Object.values(tools).forEach(el => {
             el.classList.remove('selected');
         });
-        tools.querySelector(`#${tool}`).classList.add('selected');
+        tools[tool].classList.add('selected');
+        if (emit) socket.emit('Tool Change', tool);
+    }
+
+    socket.on('Tool Change', tool => {
+        toolChange(tool);
     });
 }
 
